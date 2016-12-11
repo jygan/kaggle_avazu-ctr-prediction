8f9ad5f41d9d5baba058aa50041093d50a68efeb
Jiayan Gan <jiayangan@Jiayans-MacBook-Pro.local>
2016-12-11 15:39:05 -0800

    first commit

---
+++ preprocess.py
@@ -0,0 +1,25 @@
+import os
+import sys
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    preprocess_file = sys.argv[2]
+    test_flag = sys.argv[3]
+    print "input=" + input_file
+    print "preprocess_file=" + preprocess_file
+    output = open(preprocess_file, "w")
+    with open(input_file, "r") as lines:
+            next(lines)
+            for line in lines:
+                fields = line.split(",")
+                index = 5
+                if test_flag == 1:
+                    index = 4
+                while index < 14:
+                    fields[index] = str(hash(fields[index]) % 1000000)
+                    index += 1
+
+                newline = ",".join(fields)
+                output.write(newline)
+
+
+    output.close()
